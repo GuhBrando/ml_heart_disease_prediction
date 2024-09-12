683b0e019f16fb3aaaf16d21e9a3471d84104d77
GuhBrando <gustavo@gf2.com.br>
2024-09-11 22:25:33 -0300

    Adição de health check

--- src/config_and_setup.py
+++ src/config_and_setup.py
@@ -1,5 +1,13 @@
 import os
 
+from fastapi import APIRouter
+
 class GlobalVariables():
     def __init__(self):
         os.environ["API_VERSION"] = "/api/1.0"
+
+route = APIRouter()
+
+@route.get("/health")
+async def model_prediction():
+    return {"status": "alive"}
