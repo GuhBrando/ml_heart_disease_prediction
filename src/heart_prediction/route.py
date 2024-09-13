9a242f9130fe0fdd23570a4fe2104e42d9814f2c
GuhBrando <gustavo@gf2.com.br>
2024-09-12 22:07:13 -0300

    Adição de modelo, novas libs e pequenos ajustes

--- src/heart_prediction/route.py
+++ src/heart_prediction/route.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+import joblib
+import numpy as np
 
 from .schema import model_payload
 
@@ -6,4 +8,9 @@ route = APIRouter()
 
 @route.post("/model-prediction")
 async def model_prediction(payload: model_payload):
-    return "Alive"
+    modelo = joblib.load(open(r"./models/ModeloRandomForest.pkl", "rb"))
+
+    new_input = np.array([[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 5, 99, 2, 1, 1, 1, 4, 2, 1, 3, 1, 2, 12, 160, 78.02, 4, 1]])
+    prediction = modelo.predict(new_input)
+
+    return {"previsao": "Previsão do modelo é: " + str(prediction)}
