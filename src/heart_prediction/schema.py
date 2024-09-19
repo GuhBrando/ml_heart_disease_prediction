33d2bb430669144f2194a749a2f9182ecfbf6f68
GuhBrando <gustavo@gf2.com.br>
2024-09-18 22:12:44 -0300

    Variaveis certas do payload

--- src/heart_prediction/schema.py
+++ src/heart_prediction/schema.py
@@ -3,5 +3,26 @@ from pydantic import BaseModel
 
 class model_payload(BaseModel):
     name: str
-    price: float
-    tax: float = 10.5
+    surname: str
+    health: str
+    have_private_magic: str
+    last_checkup: str
+    last_exercise: str
+    high_blood_presure: str
+    use_cholesterol_medicine: str
+    had_a_stroke: str
+    kidney_disease: str
+    diabetes: str
+    urban_rural_status: str
+    mental_health: str
+    physical_activity: str
+    aerobic_recomendation: str
+    high_cholesterol: str
+    asthma: str
+    ethnicity: str
+    sex: str
+    age: str
+    height: str
+    weight: str 
+    smoker_status: str
+    is_heavy_drinker: str
