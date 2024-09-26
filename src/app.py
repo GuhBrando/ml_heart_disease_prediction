d306d31a2463d536f8fd189a8755620c6c56372c
GuhBrando <54749653+GuhBrando@users.noreply.github.com>
2024-09-26 00:10:34 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -8,27 +8,5 @@ from src.heart_prediction.route import route as model_prection_route
 app = FastAPI()
 global_variables = GlobalVariables()
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "*"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
 app.include_router(health_check)
 app.include_router(model_prection_route, prefix = os.environ["API_VERSION"])
