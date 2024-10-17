6de4635d1b0b9b43f823db8635e4a7ab8d8204ef
GuhBrando <gustavo@gf2.com.br>
2024-10-16 22:39:54 -0300

    ajuste de variavel

--- src/config_and_setup.py
+++ src/config_and_setup.py
@@ -15,9 +15,11 @@ class ModelPredictionError(Exception):
         super().__init__(self.message, self.name)
 
 app = FastAPI()
-
-with open("src/postgres_setup.json", "r") as read_file:
-    postgres_infos = json.load(read_file)
+try:
+    with open("src/postgres_setup.json", "r") as read_file:
+        postgres_infos = json.load(read_file)
+except:
+    pass
 os.environ["postgres_user"] = postgres_infos["username"]
 os.environ["postgres_pass"] = postgres_infos["password"]
 
